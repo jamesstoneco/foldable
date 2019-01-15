64a7133da66c27471a26cc00f1861e3916be4d37
James Stone <james.stone@futurice.com>
2019-01-15 07:54:36 +0200

    refactored to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
 import * as React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+
+import styled, { createGlobalStyle, keyframes } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const Rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Logo = styled.img`
+  animation: ${Rotate} infinite 20s linear;
+  height: 40vmin;
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppWrapper>
+        <GlobalStyle />
+        <Header>
+          <Logo src={logo} alt={"logo"} />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
+          <Link
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
+          </Link>
+        </Header>
+      </AppWrapper>
     );
   }
 }
