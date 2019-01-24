d9e7c83d7b9bee4e400fe946c10bffc71682c010
James Stone <james.stone@futurice.com>
2019-01-24 07:21:51 +0200

    patch reactstyleguidist patch from PR 1247 fix

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   propsParser: require("react-docgen-typescript").parse,
-  webpackConfig: require("react-scripts/config/webpack.config")
+  webpackConfig: require("react-scripts/config/webpack.config"),
+  dangerouslyUpdateWebpackConfig(webpackConfig, env) {
+    webpackConfig.output = {
+      ...webpackConfig.output,
+      publicPath: process.env.PUBLIC_URL || ""
+    };
+    return webpackConfig;
+  }
 };
-
 // /Users/jast/futurice-nonda/james-styled-components-demo/node_modules/react-scripts/config/webpack.config.js
 
 // /Users/jast/futurice-nonda/james-styled-components-demo/node_modules/react-scripts/config/webpackDevServer.config.js
